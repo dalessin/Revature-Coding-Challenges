adceb2fa7661681cbb242a80a00b251eebf15cea
D <nk.dalessio@gmail.com>
2022-08-03 18:44:00 -0700

    coding challenge oddEven

---
+++ oddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class oddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Give me a number, and I'll tell you if it's odd or be'in even\n");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 1) {
+            System.out.println("Odd");
+        } else {
+            System.out.println("Even");
+        }
+    }
+}
