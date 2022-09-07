1871f3d829f490576d24161f5f78cd7d490528bd
D <nk.dalessio@gmail.com>
2022-09-07 11:58:39 -0700

    longest palindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,20 +1,15 @@
-
-
-console.log(longestPalindrome("racecarsee"));
-
+console.log(longestPalindrome("address"));
 
 function longestPalindrome(String){
     //variables to hold the input and current longest palindrome found
     var inputStr = String;
     var longPal = '';
-
     for (i = 0; i <inputStr.length+1; i++){
         for (j=i; j<inputStr.length+1; j++){
             //tests the section of string between outer index (i) and incrementing inner index (j)
             var testingSection = inputStr.substring(i,j);
-            //reversed takes the above string, splits it into an array, reverses and recombines into a string
+            //reversed takes the above string, splits it into an array, reverses the contents, and recombines into a string
             var reversed = testingSection.split("").reverse().join("");
-
             //if the section being tested is identical backwards AND it's longer than the currently longest palindrome found, replace the longest palindrome with the tested section
             if ((testingSection == reversed)&&(testingSection.length > longPal.length)){
                 longPal = testingSection;
